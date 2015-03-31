64e0280398318d8ec430b4d07cf7daa0e17e9e54
Qingxiao Dong <qingxiao.dong@gmail.com>
2015-03-31 19:41:52 -0400

    Completed weight-length-scheduling algorithm.

---
+++ greedy-algorithm/weight-length-scheduling/DifferenceSchedule.java
@@ -0,0 +1,65 @@
+/**
+ * @author QingxiaoDong
+ * 
+ * Minimizes the weighted sum of completion times (the completion time of job j = sum of job lengths up
+ * to and including j) using greedy algorithm that schedules jobs in decreasing 
+ * order of the difference (weight - length). => This is a WRONG greedy algorithm
+ * 
+ * Input format:
+ * This file describes a set of jobs with positive and integral weights and lengths. It has the format
+ * [number_of_jobs]
+ * [job_1_weight] [job_1_length]
+ * [job_2_weight] [job_2_length]
+ * ...
+ * For example, the third line of the file is "74 59", indicating that the second job has weight 74 and 
+ * length 59. Edge weights or lengths may not be distinct.
+ * 
+ * Break ties: 
+ * If two jobs have equal difference (weight - length), the job with higher weight should be scheduled first. 
+ * 
+ * Output format:
+ * A positive integer which indicating the sum of weighted completion times of the resulting schedule.
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class DifferenceSchedule {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		// Scanner in = new Scanner(new File("SmallInput.txt"));
+		Scanner in = new Scanner(new File("BigInput.txt"));
+		int n = in.nextInt();
+		Comparator<int[]> comparator = new JobComparator();
+		PriorityQueue<int[]> jobs = new PriorityQueue<int[]>(n,comparator);
+		for (int i = 0; i < n; i++) {
+			int[] job = new int[2];
+			job[0] = in.nextInt();
+			job[1] = in.nextInt();
+			jobs.add(job);
+		}
+		in.close();
+		
+		long sum = 0L;
+		int currentLength = 0;
+		for (int i = 0; i < n; i++) {
+			int[] currentJob = jobs.poll();
+			currentLength = currentLength + currentJob[1];
+			sum = sum + currentJob[0] * currentLength;
+		}
+		System.out.println(sum);
+	}
+	
+	public static class JobComparator implements Comparator<int[]>
+	{
+	    @Override
+	    public int compare(int[] x, int[] y)
+	    {
+	        if (x[0] - x[1] == y[0] - y[1]) {
+	        	return y[0] - x[0];
+	        } else {
+	        	return (y[0] - y[1]) - (x[0] - x[1]);
+	        }
+	    }
+	}
+}
